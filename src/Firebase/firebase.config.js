7d41d876687feaa2dca34c0e9bee71082e98b5eb
alamin6688 <alamin.kzs06@gmail.com>
2024-04-16 16:53:26 +0600

    updated firebase config

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC8j10fw4SbK9FR_bLFfpKGPp6IqS7n5x8",
-  authDomain: "react-real-estate-auth-79472.firebaseapp.com",
-  projectId: "react-real-estate-auth-79472",
-  storageBucket: "react-real-estate-auth-79472.appspot.com",
-  messagingSenderId: "806615296775",
-  appId: "1:806615296775:web:eade6ca9c54c6cb136f3a2"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
